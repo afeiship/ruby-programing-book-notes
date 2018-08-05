12d5e869dbb7cd13534dbe688321ac7b1af32a2e
afeiship <1290657123@qq.com>
2018-08-05 22:09:28 +0800

    issue: update times

---
+++ docs/2018-08/2018-08-05/area_volume.rb
@@ -0,0 +1,20 @@
+# = begin
+# 这里可以写一大断注释
+# = end
+
+
+x = 10
+y = 20
+z = 30
+
+
+area = (x*y + y*z + z*x) * 2
+volumn = x * y * z
+
+
+print "表面积=", area, "\n";
+print "体积=", volumn, "\n";
+
+
+# cd /Users/feizheng/github/ruby-programing-book-notes/docs/2018-08
+# ruby area_volume.rb
---
+++ docs/2018-08/2018-08-05/if-else-then.rb
@@ -0,0 +1,11 @@
+a = 20;
+
+if a >= 10 then
+    print "bigger\n"
+end
+
+if a<10 then
+    print "smaller\n"
+end
+
+# 这里的then 可以省略
